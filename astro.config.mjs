a151031f3aa1f59954c0368a7f6620fa81ca949c
potato dog man <saumil707@gmail.com>
2024-11-13 10:14:40 +0800

    it builds!!!

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import vercel from '@astrojs/vercel/static';
 // Only import one vercel adapter depending on your needs
-// import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/serverless';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
